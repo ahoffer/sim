5991108e0e6f2492a580d150e757f59fdf109dac
Aaron <github@hoffer.org>
2022-06-23 16:57:34 -0700

    Second commit

--- src/test/java/com/sim/SimApplicationTests.java
+++ src/test/java/com/sim/SimApplicationTests.java
@@ -1,4 +1,4 @@
-package com.work.sim;
+package com.sim;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SimApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
